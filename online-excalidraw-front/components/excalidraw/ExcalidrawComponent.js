b3fe1dd2a01a4e0deaf1f2b55cc1190b326022bc
unknown <dys04076@naver.com>
2023-06-11 18:23:41 +0900

    add Elements, remove Elements 판별 완

--- online-excalidraw-front/components/excalidraw/ExcalidrawComponent.js
+++ online-excalidraw-front/components/excalidraw/ExcalidrawComponent.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import * as Styled from './style';
 
 const ExcalidrawComponent = () => {
   const [Excalidraw, setExcalidraw] = useState(null);
+
+  const [mouseButton, setMouseButton] = useState('up');
+  const [onChangeFlag, setOnChangeFlag] = useState(false);
   const [excalidrawAPI, setExcalidrawAPI] = useState(null);
+
+  const [preveChangeElements, setPrevChangeElements] = useState([]);
 
   useEffect(() => {
     import('@excalidraw/excalidraw').then((comp) =>
@@ -12,15 +17,58 @@ const ExcalidrawComponent = () => {
     );
   }, []);
 
+  const changeElements = useMemo(
+    () => excalidrawAPI?.getSceneElementsIncludingDeleted() || [],
+    [onChangeFlag],
+  );
+
+  const addElements = useMemo(() => {
+    return changeElements.filter((changeElement) => {
+      return !preveChangeElements.some(
+        (prevElement) => prevElement?.id === changeElement?.id,
+      );
+    });
+  }, [changeElements]);
+
+  const removeElements = useMemo(() => {
+    return changeElements.filter((changeElement) => {
+      const prevChangeElement = preveChangeElements.find(
+        (prevElement) => prevElement?.id === changeElement?.id,
+      );
+      return (
+        prevChangeElement?.isDeleted !== changeElement?.isDeleted &&
+        changeElement?.isDeleted === true
+      );
+    });
+  }, [changeElements]);
+
+  useEffect(() => {
+    // console.log('changeElements', changeElements);
+    setPrevChangeElements([...changeElements]);
+  }, [changeElements]);
+
+  //   useEffect(() => {
+  //     console.log('addElements', addElements);
+  //   }, [addElements]);
+
+  //   useEffect(() => {
+  //     console.log('removeElements', removeElements);
+  //   }, [removeElements]);
+
   return (
     <>
       <Styled.ExcalidrawLayout>
         <div style={{ width: '50%', height: '50%', border: 'solid' }}>
           {Excalidraw && (
             <Excalidraw
-              onChange={(excalidrawElements, appState, files) =>
-                console.log('elements', excalidrawElements)
-              }
+              onPointerUpdate={({ button }) => {
+                if (button != mouseButton) {
+                  if (button == 'up' && mouseButton == 'down') {
+                    setOnChangeFlag(!onChangeFlag);
+                  }
+                  setMouseButton(button);
+                }
+              }}
               ref={(api) => setExcalidrawAPI(api)}
             />
           )}
